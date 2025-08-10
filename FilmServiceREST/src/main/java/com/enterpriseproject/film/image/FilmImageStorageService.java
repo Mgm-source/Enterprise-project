e702f53b76925f37a0af9263515c365a9cb36070
Munashe Matadi <Munashe.m@outlook.com>
2025-08-10 13:20:22 +0100

    unused import

--- FilmServiceREST/src/main/java/com/enterpriseproject/film/image/FilmImageStorageService.java
+++ FilmServiceREST/src/main/java/com/enterpriseproject/film/image/FilmImageStorageService.java
@@ -11,7 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 @Service
