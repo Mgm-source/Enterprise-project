fb5bc422747a33e8e33fd9ce4f1f2234510b5f9e
Munashe Matadi <Munashe.m@outlook.com>
2025-08-10 11:23:18 +0100

    test

--- FilmServiceREST/src/main/java/com/enterpriseproject/film/Films.java
+++ FilmServiceREST/src/main/java/com/enterpriseproject/film/Films.java
@@ -3,13 +3,12 @@ package com.enterpriseproject.film;
 import java.util.List;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
-
-import jakarta.xml.bind.annotation.XmlElement;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
 public class Films {
 	
 	@JacksonXmlElementWrapper(useWrapping = false)
-	@XmlElement(name = "film")
+	@JacksonXmlProperty(localName = "film")  
 	private List<Film> filmlist;
 	
 	public void setFilmList(List<Film> filmlist) {
