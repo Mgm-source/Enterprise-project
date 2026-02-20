713c5304332aa677617f405e77938124ed5f8a21
Munashe Matadi <munashe.m@outlook.com>
2026-02-20 22:06:21 +0000

    poop super secure

---
+++ filmservicerest/src/main/java/com/enterpriseproject/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.enterpriseproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean 
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorize -> authorize
+                .anyRequest().authenticated()
+            )
+            .httpBasic(Customizer.withDefaults())
+            .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userService() {
+        UserDetails user = User.withUsername("guest")
+            .password("{noop}password123") 
+            .roles("default")
+            .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
